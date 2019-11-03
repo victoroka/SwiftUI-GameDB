42e54389c0155fef53203b014ac3be0099c2489b
Victor Oka <victoroka@Victors-MacBook-Pro.local>
2019-11-03 12:08:56 -0300

    redesigned game detail view

--- SwiftUI-GameDB/Models/GameDetail.swift
+++ SwiftUI-GameDB/Models/GameDetail.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+class GameDetail: ObservableObject {
+    
+    @Published var game: Game? = nil
+    @Published var isLoading = false
+    
+    var gameService: GameService = GameStore.shared
+    
+    func reload(id: Int) {
+        self.isLoading = true
+        
+        self.gameService.fetchGame(id: id) { [weak self] (result) in
+            self?.isLoading = false
+            
+            switch result {
+            case .success(let game):
+                self?.game = game
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
--- SwiftUI-GameDB/Models/GameDetailView.swift
+++ SwiftUI-GameDB/Models/GameDetailView.swift
@@ -8,10 +8,80 @@
 
 import SwiftUI
 
+// MARK: Main Game Detail View
 struct GameDetailView: View {
     
+    @ObservedObject var gameDetail: GameDetail = GameDetail()
+    @ObservedObject var imageLoader: ImageLoader = ImageLoader()
+    
     var gameId: Int
+    
     var body: some View {
-        Text(String(gameId))
+        Group {
+            if self.gameDetail.game != nil {
+                List {
+                    PosterView(image: self.imageLoader.image)
+                        .onAppear {
+                            if let url = self.gameDetail.game?.coverURL {
+                                self.imageLoader.downloadImage(url: url)
+                            }
+                        }
+                    GameSectionView(game: self.gameDetail.game!)
+                }
+            } else {
+                LoadingView()
+            }
+        }
+        .edgesIgnoringSafeArea([.top])
+        .onAppear {
+            self.gameDetail.reload(id: self.gameId)
+        }
+    }
+}
+
+// MARK: Poster View
+struct PosterView: View {
+    
+    var image: UIImage?
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.gray)
+                .aspectRatio(500/750, contentMode: .fit)
+            
+            if image != nil {
+                Image(uiImage: self.image!)
+                    .resizable()
+                    .aspectRatio(500/750, contentMode: .fit)
+            }
+        }
+    }
+}
+
+// MARK: Game Section View
+struct GameSectionView: View {
+    
+    var game: Game
+    
+    var body: some View {
+        Section {
+            Text(game.summary)
+                .font(.body)
+                .lineLimit(nil)
+            
+            if !game.storyline.isEmpty {
+                Text(game.storyline)
+                    .font(.body)
+                    .lineLimit(nil)
+            }
+            
+            Text(game.genreText)
+                .font(.subheadline)
+            Text(game.releaseDateText)
+                .font(.subheadline)
+            Text(game.company)
+                .font(.subheadline)
+        }
     }
 }
